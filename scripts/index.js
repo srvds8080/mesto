196ae8bcbd551fb1bd665c7951488032233028ae
srvds8080 <srvds@yandex.ru>
2020-07-29 19:28:52 +0300

    to do render card

--- scripts/index.js
+++ scripts/index.js
@@ -16,6 +16,52 @@ const editFormDescription = editForm.querySelector(".popup__form-input_type_desc
 
 const saveProfileEditButton = popupWindow.querySelector(".popup__form-btn");//  сохранить проект button
 
+const elements = document.querySelector('.elements');
+
+const initialCards = [
+	{
+		 name: 'Архыз',
+		 link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+	},
+	{
+		 name: 'Челябинская область',
+		 link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+	},
+	{
+		 name: 'Иваново',
+		 link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+	},
+	{
+		 name: 'Камчатка',
+		 link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+	},
+	{
+		 name: 'Холмогорский район',
+		 link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+	},
+	{
+		  name: 'Байкал',
+		  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+	}
+];
+
+
+
+initialCards.forEach((data) => {
+	const cardTemplateContent = document.querySelector('.card-content').content.querySelector('.card-box');
+	const cardBox = cardTemplateContent.cloneNode(true);
+	const cardImg = cardBox.querySelector('.card-box__img');
+	const cardTitle = cardBox.querySelector('.card-box__text');
+	const cardLike = cardBox.querySelector('.card-box__button');
+	const cardDelete = cardBox.querySelector('.card-box__delete');
+
+	cardImg.src = data.link;
+	cardImg.alt = data.name;
+	cardTitle.textContent = data.name;
+	elements.prepend(cardBox);
+})
+
+
 function openPopup() {
 	editFormName.value = profileName.textContent;
 	editFormDescription.value = profileDescription.textContent;
@@ -36,3 +82,10 @@ function saveForm(evt) {
 profileEditButtton.addEventListener('click', openPopup); // открыть окна
 closeProfileEditButton.addEventListener('click', closePopup);// закрыть окно
 editForm.addEventListener('submit', saveForm);// активация кнопки сохранить
+
+
+const openAddCardModalButton = document.querySelector('.profile__add-btn');
+
+openAddCardModalButton.addEventListener('click', () => {
+	
+})
